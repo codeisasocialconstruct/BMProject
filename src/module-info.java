c54204557392bec0eb0dec9bfef4267d6c0d52bf
codeisasocialproject <bartosz.sobieski.1998@wp.pl>
2019-04-22 22:41:08 +0200

    Project reconfigured to Java 1.8. Added sample animation of Ultra-Rapid-Fast Cat

--- src/module-info.java
+++
@@ -1,6 +0,0 @@
-module Battle.Metropolis {
-    requires javafx.fxml;
-    requires javafx.controls;
-
-    opens sample;
-}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,24 +1,52 @@
 package sample;
 
+import javafx.animation.Animation;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
+public class Main extends Application
+{
 
-public class Main extends Application {
+    private final Image IMAGE = new Image(getClass().getResourceAsStream("1.png"));
+
+    private static final int COLUMNS = 4;
+    private static final int COUNT = 8;
+    private static final int OFFSET_X = 0;
+    private static final int OFFSET_Y = 0;
+    private static final int WIDTH = 512;
+    private static final int HEIGHT = 256;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception
+    {
+        primaryStage.setTitle("The Horse in Motion");
+
+        final ImageView imageView = new ImageView(IMAGE);
+        imageView.setViewport(new Rectangle2D(OFFSET_X, OFFSET_Y, WIDTH, HEIGHT));
+
+        final Animation animation = new SpriteAnimation(
+                imageView,
+                Duration.millis(500),
+                COUNT, COLUMNS,
+                OFFSET_X, OFFSET_Y,
+                WIDTH, HEIGHT
+        );
+        animation.setCycleCount(Animation.INDEFINITE);
+        animation.play();
+
+        primaryStage.setScene(new Scene(new Group(imageView)));
         primaryStage.show();
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch(args);
     }
 }
