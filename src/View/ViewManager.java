4f508a41852fc65d9756b2f6b474f0a7318b7e8d
inaccuratecode <trolmaster09@gmail.com>
2019-04-28 12:33:56 +0200

    Created background, added new buttons

--- src/View/ViewManager.java
+++ src/View/ViewManager.java
@@ -1,9 +1,13 @@
 package View;
 
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import Model.NavigationButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewManager {
 
@@ -12,24 +16,39 @@ public class ViewManager {
     private AnchorPane mainPane;
     private Scene mainScene;
     private Stage mainStage;
+    private final int MENU_BUTTON_START_X = 100;
+    private final int MENU_BUTTON_START_Y = 100;
+
+    private List<NavigationButton> menuButtons;
 
     public ViewManager() {
+        menuButtons = new ArrayList<>();
         mainPane = new AnchorPane();
         mainScene = new Scene(mainPane, WIDTH, HEIGHT);
         mainStage = new Stage();
 
         mainStage.setScene(mainScene);
-        CreateButtons();
+        CreateButtons(new NavigationButton("PLAY"));
+        CreateButtons(new NavigationButton("HELP"));
+        CreateButtons(new NavigationButton("OPTIONS"));
+        CreateButtons(new NavigationButton("EXIT"));
+        CreateBackground();
     }
 
     public Stage getMainStage() {
         return mainStage;
     }
 
-    private void CreateButtons() {
-        NavigationButton button = new NavigationButton("Click!");
-        button.setLayoutX(100);
-        button.setLayoutY(100);
+    private void CreateButtons(NavigationButton button) {
+        button.setLayoutX(MENU_BUTTON_START_X);
+        button.setLayoutY(MENU_BUTTON_START_Y + menuButtons.size()*100);
+        menuButtons.add(button);
         mainPane.getChildren().add(button);
+    }
+
+    private void CreateBackground() {
+        Image backgroundMenuImage = new Image("View/resources/temp_background.png", WIDTH, HEIGHT, false, true);
+        BackgroundImage backgroundMenu = new BackgroundImage(backgroundMenuImage, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, null);
+        mainPane.setBackground(new Background(backgroundMenu));
     }
 }
