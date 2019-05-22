6f7ed06412ec6ccc9c6a704ecb553cb2b50fa81e
BSobieski <bartosz.sobieski.1998@wp.pl>
2019-05-22 17:27:49 +0200

    Changed in-game music

--- src/View/MusicManager.java
+++ src/View/MusicManager.java
@@ -4,7 +4,7 @@ import javafx.scene.media.AudioClip;
 
  class MusicManager {
     final static String MENU_THEME = "resources/Sounds/title_theme.mp3";
-    final static String MAIN_THEME = "resources/Sounds/mecha_collection.mp3";
+    final static String MAIN_THEME = "resources/Sounds/Doot.mp3";
     final static String CLICK_SOUND = "resources/Sounds/clickSound.mp3";
 
     private AudioClip currentlyPlaying;
